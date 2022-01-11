c82d790252300712ebfcc4d0a25b7c28b99660df
Alex Zhang <alexzzf@Alexs-MacBook-Pro.local>
2022-01-11 18:43:16 -0500

    Adding the election analysis

--- Pypoll.py
+++ Pypoll.py
@@ -10,10 +10,14 @@ import os
 #with statement to open the file
 
 file_to_load= os.path.join("Resources","election_results.csv")
-files_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 total_votes = 0
 candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count =0
+winning_percentage =0
 
 with open(file_to_load) as election_data:
 
@@ -29,9 +33,45 @@ with open(file_to_load) as election_data:
         candidate_name =row[2]
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
+            candidate_votes[candidate_name]=0
+        candidate_votes[candidate_name]+=1
 
-print(total_votes)
-print(candidate_options)
+with open(file_to_save, "w") as txt_file:
+
+    election_results= (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
+
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes)/float(total_votes)*100
+        
+        candidate_results =(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
+
+        if (votes>winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"-------------------\n"
+        f"Winner:{winning_candidate}\n"
+        f"Winning Vote Count:{winning_count:,}\n"
+        f"winning Percentage:{winning_percentage:.1f}%\n"
+        f"-------------------\n")
+
+    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
+
+
+
+
 
 
 
